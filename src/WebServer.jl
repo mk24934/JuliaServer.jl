6ea97c38445f8bf5051e4700234a981349178978
mk24934 <amgs24934@gmail.com>
2023-03-29 11:12:51 +0000

    add Genie webserver

--- src/WebServer.jl
+++ src/WebServer.jl
@@ -1,7 +1,15 @@
 module WebServer
 
+using Genie
+
+route("/hello") do
+  "Hello World"
+end
+
+up(8000)
+
 function hello_world()
-  print("Hello World from main structs!")
+  print("Hello World from Web Server!!")
 end
 
 end
